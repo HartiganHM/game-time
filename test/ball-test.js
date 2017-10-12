cb9622806e47c87e18a83388f408927afbf3c38a
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-11 20:21:19 -0600

    Removed instances of Ball in each test code

--- test/ball-test.js
+++ test/ball-test.js
@@ -13,49 +13,41 @@ it('should be a function', function () {
 });
 
 it('should have a starting x', function () {
-  var ball = new Ball(5);
   assert.equal(ball.x, 5);
 
 });
 
 it('should have a starting y', function () {
-  var ball = new Ball(5, 200);
   assert.equal(ball.y, 200);
 
 });
 
 it('should a radius start', function () {
-  var ball = new Ball(5, 200, 3);
   assert.equal(ball.rStart, 3);
 
 });
 
 it('should have a radius end', function () {
-  var ball = new Ball(5, 200, 3, 5);
   assert.equal(ball.rEnd, 5);
 
 });
 
 it('should have a pi', function () {
-  var ball = new Ball(5, 200, 3, 5, 2);
   assert.equal(ball.pi, 2);
 
 });
 
 it('should have counterclockwise rotation', function () {
-  var ball = new Ball(5, 200, 3, 5, 2, true);
   assert.equal(ball.counterclockwise, true);
 
 });
 
 it('should have an x-velocity', function () {
-  var ball = new Ball(5, 200, 3, 5, 2, true);
   assert.equal(ball.dx, 0);
 
 });
 
 it('should have a y-velocity', function () {
-  var ball = new Ball(5, 200, 3, 5, 2, true);
   assert.equal(ball.dy, 0);
 
 });
